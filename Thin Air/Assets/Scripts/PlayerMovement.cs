9b441ec96fdf763908286a49ce56fb3bd9ad0cc4
anguyen415 <nguyenandrew1998@outlook.com>
2018-10-01 11:22:54 -0500

    Added Simple PlayerMovement
    PlayerMovement Script added.
    Simple scene with Box Object.
    Need to fix y velocity.

---
+++ Thin Air/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private bool _useFixedUpdateForInput = false;
+
+    [SerializeField]
+    private float _speed = 3.0f;
+
+    private static float HorizontalInput
+    {
+        get { return Input.GetAxis("Horizontal"); }
+    }
+
+    private static float VerticalInput
+    {
+        get { return Input.GetAxis("Vertical"); }
+    }
+
+    private static Vector2 UserInput
+    {
+        get { return new Vector2(HorizontalInput, VerticalInput); }
+    }
+
+    private Vector3 _inputDirection;
+    private Rigidbody _rigidBody;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponentInChildren<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (!_useFixedUpdateForInput)
+        {
+            _inputDirection = UserInput;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_useFixedUpdateForInput)
+        {
+            _inputDirection = UserInput;
+        }
+
+        _rigidBody.velocity = new Vector3(_inputDirection.x, 0.0f, _inputDirection.y) * _speed;
+    }
+}
