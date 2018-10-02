1624879fd7234eb28f3ed380202dba67247c2c22
anhle697 <32230670+anhle697@users.noreply.github.com>
2018-10-02 16:06:53 -0500

    Added Camera follow (prototype)

---
+++ Thin Air/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    [SerializeField]
+    private Transform target_player;
+    [SerializeField]
+    private float height = 10f;
+    [SerializeField]
+    private float distance = 20f;
+    [SerializeField]
+    private float angle = 45f;
+    private Vector3 refVelocity;
+	// Use this for initialization
+	void Start () {
+        HandleCamera();
+ 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleCamera();
+
+    }
+
+    protected void HandleCamera()
+    {
+        if (!target_player)
+        {
+            return;
+        }
+        Vector3 worldPosition = (Vector3.forward * distance) + (Vector3.up * height);
+        Vector3 rotatedVector = Quaternion.AngleAxis(angle, Vector3.up) * worldPosition;
+        Vector3 flatPosition = (target_player.position);
+        flatPosition.y = 0f;
+        Vector3 finalPosition = flatPosition + rotatedVector;
+        transform.position = Vector3.SmoothDamp(transform.position, finalPosition, ref refVelocity, .5f);
+        transform.LookAt(flatPosition);
+    }
+}
---
+++ Thin Air/Assets/Scripts/PlayerMovement_anh.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_anh : MonoBehaviour
+{
+ 
+    private CharacterController controller;
+    [SerializeField]
+    private float speed = 5;
+    [SerializeField]
+    private float jumpHeight = 2f;
+    [SerializeField]
+    private float gravity = 9;
+    private Vector3 velocity;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+
+        }
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * speed);
+        if (move != Vector3.zero)
+        {
+            transform.forward = move;
+        }
+        if (Input.GetButton("Jump") && controller.isGrounded)
+        {
+            velocity.y = jumpHeight;
+        }
+        velocity.y -= gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+        
+
+}
