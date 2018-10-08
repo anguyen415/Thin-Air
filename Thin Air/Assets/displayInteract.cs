b98e6cd176ae73e64da07cef39a7bdc41610c8f2
j-davalos <43656051+j-davalos@users.noreply.github.com>
2018-10-08 14:33:38 -0500

    started "Press E to Interact"

---
+++ Thin Air/Assets/displayInteract.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class displayInteract : MonoBehaviour {
+	Collider col;
+
+	// Use this for initialization
+	void Start ()
+	{
+		col = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		GameObject.FindGameObjectWithTag("interUI").SetActive(true);
+	}
+	void OnTriggerExit(Collider col)
+	{
+		GameObject.FindGameObjectWithTag("interUI").SetActive(false);
+	}
+}
