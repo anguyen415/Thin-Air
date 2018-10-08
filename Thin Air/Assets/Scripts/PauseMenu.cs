fd6ebaf93d9f378bed86529d61191770ae4eb439
anguyen415 <nguyenandrew1998@outlook.com>
2018-10-08 02:05:33 -0500

    Added PauseMenu and Restarting UI and Functionality
    Did not update InfirmaryLevel.unity and .meta -- Will add them into scene after pulling changes.

---
+++ Thin Air/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    bool isAlive = true;
+
+
+    [SerializeField]
+    private Text DeathText;
+
+
+    [SerializeField]
+    private GameObject ResumeButton;
+	// Use this for initialization
+	void Start () {
+        Resume();
+        DeathText.gameObject.SetActive(false);
+        ResumeButton.SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } 
+            else
+            {
+                Pause();
+            }
+        }
+        /*if ((player.GetComponent("Oxygen") as Oxygen).CurrentOxygen == 0)
+        {
+            restartButton.SetActive(true);
+        }
+        else
+        {
+            restartButton.SetActive(false);
+        }*/
+	}
+
+    void FixedUpdate()
+    {
+        
+        if (Oxygen.CurrentOxygen <= 0)
+        {
+            ResumeButton.SetActive(false);
+            DeathText.gameObject.SetActive(true);
+            Pause();
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+    public void RestartLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
